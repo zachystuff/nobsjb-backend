5379d813238c7b1f31ab2c208ecbb88901ba0a84
Michael  Stack <mstack000@gmail.com>
2021-12-10 18:03:21 -0500

    finished removal and restructure
    removed .then, added async await. added try catch and some console logging for debugging purposes.

--- db.js
+++ db.js
@@ -14,37 +14,43 @@ function initialize() {
 
 const jobDb = {
 
-    addJobListing: (req) => {
-
-        jobsCollection.insertOne(req.body)
-            .then(result => {
+    addJobListing: async(req) => {
+        try {
+            if (Object.keys(req.body).length) {
+                let result = await jobsCollection.insertOne(req.body);
                 console.log(result);
-            })
-            .catch(error => console.error(error))
+            }
+        } catch (error) {
+            console.log(error);
+        }
     },
 
     readJobListing: async(req) => {
         //returns all jobs in the collection if req is empty
-        console.log(req.body);
-        if (!Object.keys(req.body).length) {
-            console.log('empty body request')
-            let result = await jobsCollection.find().toArray();
-            console.log(result);
-            return result;
-        } else {
-            console.log('full body request')
-            let result = await jobsCollection.find(req.body).toArray();
-            return result;
+        try {
+            console.log(req.body);
+            if (!Object.keys(req.body).length) {
+                console.log('empty body request')
+                let result = await jobsCollection.find().toArray();
+                console.log(result);
+                return result;
+            } else {
+                console.log('full body request')
+                let result = await jobsCollection.find(req.body).toArray();
+                return result;
+            }
+        } catch (error) {
+            console.log(error);
         }
     },
 
-    deleteJobListing: (req) => {
-
-        jobsCollection.deleteOne(req.id)
-            .then(result => {
-                console.log(result);
-            })
-            .catch(error => console.error(error))
+    deleteJobListing: async(req) => {
+        try {
+            let result = await jobsCollection.deleteOne(req.id);
+            return result;
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
 
