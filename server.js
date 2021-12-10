26fb28b4d6fcb27e34d8ca6e5326d39fc650fd42
Michael  Stack <mstack000@gmail.com>
2021-12-10 13:06:15 -0500

    fixed endpoint res
    see summary

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv').config();
+const { initializeApp } = require('firebase-admin/app');
 const express = require('express');
 const server = express();
 const bodyParser = require('body-parser');
@@ -6,28 +7,79 @@ const PORT = 3000;
 const cors = require('cors');
 const mongo = require('./db');
 
+
 mongo.initialize();
 server.use(cors());
+
+initializeApp({
+    credential: applicationDefault(),
+    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+});
+
+getAuth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+        const uid = decodedToken.uid;
+        // ...
+    })
+    .catch((error) => {
+        // Handle error
+    });
 
 server.listen(PORT);
 
 server.use(bodyParser.urlencoded({ extended: true }));
 
-server.get('/', (req, res) => {
+server.get('/favorites', (req, res) => {
     console.log('get');
-    res.send('Hello World!');
+    //get from user, favorite IDs and return jobs with fav IDs
+    res.send('got favorites');
 });
 
-server.post('/', (req, res) => {
+
+server.get('/find-jobs', (req, res) => {
+    //queries user and jobs, returns jobs based on user
+    //if empty request return all jobs
+    //else give body params
+    res.send('found jobs');
+})
+
+server.get('/profile/:ID', (req, res) => {
+    //queries user and jobs, returns jobs based on user
+    //if empty request return all jobs
+    //else give body params
+    res.send('user returned');
+})
+
+server.post('/create-job', (req, res) => {
     mongo.addData(req)
+        //creates job based on form inputs post validation
     res.send('jobs done');
 })
 
-server.delete('/', (req, res) => {
+server.delete('/favorite/:ID', (req, res) => {
     mongo.deleteJob(req)
-    res.send('jobs gone');
+        //deletes job ID from user favorites
+    res.send('jobs gone from favorites');
 })
 
-server.put('/', (req, res) => {
-    res.send('jobs replaced');
+server.put('/favorite/:ID', (req, res) => {
+    //adds job ID to user favorites list in Mongo
+    res.send('job added to favorites');
+})
+
+server.put('/apply/:ID', (req, res) => {
+    //adds job ID to user applied list in Mongo with epoch time that request was sent
+    res.send('job added to applied list');
+})
+
+server.put('/ignore/:ID', (req, res) => {
+    //adds job ID to user ignored list in Mongo
+    //returns list of jobs minus jobs in the ignored list
+    res.send('job ignored');
+})
+
+server.put('/profile', (req, res) => {
+    //adds user to Mongo with default app, fav, and ignore lists (IE: EMPTY LISTS)
+    res.send('user added');
 })
